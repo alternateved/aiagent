ed1bfdce74b1409b8f0d89e373e1237e6a44a10b
Tomasz Hołubowicz <mail@alternateved.com>
2025-07-03 21:50:12 +0200

    Allow prompt from input

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
 def main():
-    print("Hello from aiagent!")
+
+    if len(sys.argv) < 2:
+        print("Usage: uv run main.py <prompt>")
+        sys.exit(1)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=sys.argv[1],
+    )
+    print(f"Response: {response.text}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
