f0a6d126f1b497bb894bbf48b2efc3a83e766d51
Tomasz Hołubowicz <mail@alternateved.com>
2025-07-03 22:24:30 +0200

    Add verbose mode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,16 +12,23 @@ client = genai.Client(api_key=api_key)
 def main():
 
     if len(sys.argv) < 2:
-        print("Usage: uv run main.py <prompt>")
+        print("Usage: uv run main.py <prompt> [--verbose]")
         sys.exit(1)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1],
+        contents=messages,
     )
+
     print(f"Response: {response.text}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {sys.argv[1]}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
