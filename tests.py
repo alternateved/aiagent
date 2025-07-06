54e5f040617ce120fa181329ab87193da802b4c6
Tomasz Hołubowicz <mail@alternateved.com>
2025-07-07 00:06:43 +0200

    Update tests

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+from functions.write_file import write_file
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 
@@ -14,5 +15,17 @@ files = ["lorem.txt", "main.py", "pkg/calculator.py", "/bin/cat"]
 for file in files:
     result = get_file_content("calculator", file)
     print(f"Result for {file} file:")
+    print(result)
+    print("\n")
+
+print("Tests for write_file")
+tuples = [
+    ("lorem.txt", "wait, this isn't lorem ipsum"),
+    ("pkg/morelorem.txt", "lorem ipsum dolor sit amet"),
+    ("/tmp/temp.txt", "this should not be allowed"),
+]
+for tuple in tuples:
+    result = write_file("calculator", tuple[0], tuple[1])
+    print(f"Result for {tuple[0]} file:")
     print(result)
     print("\n")
